ebf3d43569efb0a297afc4972d061ae525e4dfe2
yoonkwonsong <78392347+yoonkwonsong@users.noreply.github.com>
2023-04-18 14:09:33 +0900

    4

--- app.js
+++ app.js
@@ -4,15 +4,13 @@ document.querySelectorAll(".btn").forEach((btn) => {
             .querySelectorAll(".card")
             .forEach((c) => c.classList.remove("open-card"))
 
-        if (e.currentTarget.classList.contains("btn-plus")) {
+        e.currentTarget.classList.contains("btn-plus") &&
             e.currentTarget.parentElement.parentElement.parentElement.classList.add(
                 "open-card"
             )
-        }
-        if (e.currentTarget.classList.contains("btn-minus")) {
+        e.currentTarget.classList.contains("btn-minus") &&
             e.currentTarget.parentElement.parentElement.parentElement.classList.remove(
                 "open-card"
             )
-        }
     })
 })
